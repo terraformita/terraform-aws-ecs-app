9a2c7980919a3ebcf507627d356a25889c50e01c
Constantine Kurianoff <constantine@kurianoff.pro>
2024-03-13 11:55:19 -0400

    fix: handling empty container hostname

--- main.tf
+++ main.tf
@@ -36,9 +36,9 @@ locals {
       "{db_user}"                  = local.db_enabled ? local.db_user : ""
       "{db_password}"              = local.db_enabled ? random_password.db_password[0].result : ""
       "{cognito_region}"           = try(var.region, "us-east-1")
-      "{cognito_user_pool_arn}"    = contains(local.host_based_user_pools, container.hostname) ? aws_cognito_user_pool.host_based[container.hostname].arn : (local.create_user_pool ? aws_cognito_user_pool.user_pool[0].arn : "")
-      "{cognito_user_pool_id}"     = contains(local.host_based_user_pools, container.hostname) ? aws_cognito_user_pool.host_based[container.hostname].id : (local.create_user_pool ? aws_cognito_user_pool.user_pool[0].id : "")
-      "{cognito_user_pool_domain}" = contains(local.host_based_user_pools, container.hostname) ? aws_cognito_user_pool.host_based[container.hostname].domain : (local.create_user_pool ? aws_cognito_user_pool.user_pool[0].domain : "")
+      "{cognito_user_pool_arn}"    = try(contains(local.host_based_user_pools, container.hostname), false) ? aws_cognito_user_pool.host_based[container.hostname].arn : (local.create_user_pool ? aws_cognito_user_pool.user_pool[0].arn : "")
+      "{cognito_user_pool_id}"     = try(contains(local.host_based_user_pools, container.hostname), false) ? aws_cognito_user_pool.host_based[container.hostname].id : (local.create_user_pool ? aws_cognito_user_pool.user_pool[0].id : "")
+      "{cognito_user_pool_domain}" = try(contains(local.host_based_user_pools, container.hostname), false) ? aws_cognito_user_pool.host_based[container.hostname].domain : (local.create_user_pool ? aws_cognito_user_pool.user_pool[0].domain : "")
       "{cognito_client_id}"        = try(aws_cognito_user_pool_client.endpoint_centralized[name].id, try(aws_cognito_user_pool_client.host_based[container.hostname].id, (local.create_user_pool ? aws_cognito_user_pool_client.user_pool[0].id : "")))
       "{cognito_client_secret}"    = try(aws_cognito_user_pool_client.endpoint_centralized[name].client_secret, try(aws_cognito_user_pool_client.host_based[container.hostname].client_secret, (local.create_user_pool ? aws_cognito_user_pool_client.user_pool[0].client_secret : "")))
     }
